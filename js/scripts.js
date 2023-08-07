3b3e2720309b31b517cd72b4d36ad666e5fa45c0
luckycarvalho <126518568+luckycarvalho@users.noreply.github.com>
2023-08-07 16:36:43 -0300

    upadate

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,4 @@
-// document.addEventListener('DOMContentLoaded', () =>{
-//     const form = document.querySelector('#form');
-//     const emailInput = document.querySelector('#email');
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     const errorSpan = document.querySelector('#invalid');
-//     const emailSpan = document.querySelector('.displayEmail');
-
-//     form.addEventListener('submit', (event) => {
-//         event.preventDefault();
-//         //Verifica se o email está vazio ou inválido
-//         if (emailInput.value === "" || !emailRegex.test(emailInput.value)) {
-//             errorSpan.innerHTML = "Valid email required";
-//             emailInput.parentElement.classList.add('invalid');
-//         } else {
-//             localStorage.setItem('userEmail', emailInput.value);
-//             window.location.href = 'success.html';
-//         }
-        
-//     });
-
-//     emailInput.addEventListener('input', () => {
-//         errorSpan.textContent = "";
-//         emailInput.parentElement.classList.remove('invalid');
-//     });
-
-//     const savedEmail = localStorage.getItem('userEmail');
-//     document.querySelector('.displayEmail').textContent = savedEmail;
-// });
-
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     const form = document.querySelector('#form');
     const emailInput = document.querySelector('#email');
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
