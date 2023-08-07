4141722e70606c3437ea3cf7a5956bb1e5fd329e
Matheus Carvalho <126518568+luckycarvalho@users.noreply.github.com>
2023-08-07 16:19:39 -0300

    correção do script
    Anteriormente o script não estava exibindo o e-mail na página success.html. Com esta correção, agora ele está sendo exibido normalmente

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,57 @@
-const form = document.querySelector('#form');
-const emailInput = document.querySelector('#email');
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const errorSpan = document.querySelector('#invalid');
-const emailSpan = document.querySelector('.email-user');
+// document.addEventListener('DOMContentLoaded', () =>{
+//     const form = document.querySelector('#form');
+//     const emailInput = document.querySelector('#email');
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     const errorSpan = document.querySelector('#invalid');
+//     const emailSpan = document.querySelector('.displayEmail');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    //Verifica se o email está vazio ou inválido
-    if (emailInput.value === "" || !emailRegex.test(emailInput.value)) {
-        errorSpan.innerHTML = "Valid email required";
-        emailInput.parentElement.classList.add('invalid');
-    } else {
-        localStorage.setItem('userEmail', emailInput.value);
-        window.location.href = 'success.html';
-    }
-    
+//     form.addEventListener('submit', (event) => {
+//         event.preventDefault();
+//         //Verifica se o email está vazio ou inválido
+//         if (emailInput.value === "" || !emailRegex.test(emailInput.value)) {
+//             errorSpan.innerHTML = "Valid email required";
+//             emailInput.parentElement.classList.add('invalid');
+//         } else {
+//             localStorage.setItem('userEmail', emailInput.value);
+//             window.location.href = 'success.html';
+//         }
+        
+//     });
+
+//     emailInput.addEventListener('input', () => {
+//         errorSpan.textContent = "";
+//         emailInput.parentElement.classList.remove('invalid');
+//     });
+
+//     const savedEmail = localStorage.getItem('userEmail');
+//     document.querySelector('.displayEmail').textContent = savedEmail;
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#form');
+    const emailInput = document.querySelector('#email');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const errorSpan = document.querySelector('#invalid');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // Verifica se o email está vazio ou inválido
+        if (emailInput.value === "" || !emailRegex.test(emailInput.value)) {
+            errorSpan.innerHTML = "Valid email required";
+            emailInput.parentElement.classList.add('invalid');
+        } else {
+            //Salva o email em userEmail e redireciona o usuário para outra página
+            localStorage.setItem('userEmail', emailInput.value);
+            window.location.href = 'success.html';
+        }
+    });
+
+    emailInput.addEventListener('input', () => {
+        errorSpan.textContent = "";
+        emailInput.parentElement.classList.remove('invalid');
+    });
 });
-
-emailInput.addEventListener('input', () => {
-    errorSpan.textContent = "";
-    emailInput.parentElement.classList.remove('invalid');
-});
-
-const userEmail = localStorage.getItem('userEmail');
-if (userEmail) {
-    document.querySelector('.email-user').textContent = userEmail;
- }
+  
 
 
 
